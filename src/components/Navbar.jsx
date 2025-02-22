d6d01946aeb54e4736b3f9b944a557e8a07e9eb6
Sanskriti <sanskritisri27@gmail.com>
2025-02-22 14:34:15 +0530

    built routes for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Link, Flex, HStack, Box } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <Flex justifyContent={"space-between"} padding={8} fontSize={25} fontWeight={'bold'}>
+      <Box>My Store</Box>
+      <HStack gap={8}>
+        <Link as={RouterLink} to="/">
+          Products
+        </Link>
+        <Link as={RouterLink} to="/cart">
+          Cart
+        </Link>
+      </HStack>
+    </Flex>
+  );
+};
 
-export default Navbar
+export default Navbar;
