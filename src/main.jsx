bf1f5634bcfb58ee88d18642e75df449e9653d28
Sanskriti <sanskritisri27@gmail.com>
2025-02-22 14:25:52 +0530

    added folder structure as defined in the assignment

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider as ChakraUIProvider } from "./components/ui/provider";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <ChakraUIProvider>
+        <App />
+      </ChakraUIProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
